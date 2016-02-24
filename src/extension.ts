eef3b2d03089bb67bb9ada109832cef78f775863
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2016-02-24 23:27:58 +0100

    Add command listeners

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,31 @@
 "use strict";
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let commands: vscode.Disposable[] = [];
+    commands.push(
+        vscode.commands.registerCommand("gerrit.checkOutBranch", () => {
+            vscode.window.showInformationMessage("Gerrit: Check out branch");
+        })
+    );
+    commands.push(
+        vscode.commands.registerCommand("gerrit.checkOutRevision", () => {
+            vscode.window.showInformationMessage("Gerrit: Check out revision");
+        })
+    );
+    commands.push(
+        vscode.commands.registerCommand("gerrit.commitAmend", () => {
+            vscode.window.showInformationMessage("Gerrit: Commit amend");
+        })
+    );
+    commands.push(
+        vscode.commands.registerCommand("gerrit.pushBranch", () => {
+            vscode.window.showInformationMessage("Gerrit: Push to branch");
+        })
+    );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-    console.log("Congratulations, your extension 'gerrit-vscode' is now active!");
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand("extension.activateGerrit", () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-        vscode.window.showInformationMessage("Hello Gerrit!");
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.concat(commands);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
