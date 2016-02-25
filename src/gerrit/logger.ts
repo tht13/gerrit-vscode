21937ffaec9cb405715286bc4881675c95c50939
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2016-02-25 23:19:09 +0100

    Add a OutputChannel logger

---
+++ src/gerrit/logger.ts
@@ -0,0 +1,29 @@
+import { window, OutputChannel } from "vscode";
+
+export class Logger {
+    private static _logger: LoggerSingleton = null;
+
+    get logger() {
+        if (Logger._logger === null) {
+            Logger._logger = new LoggerSingleton();
+        }
+        return Logger._logger;
+    }
+
+}
+
+class LoggerSingleton {
+    private outputChannel: OutputChannel;
+    private visible: boolean = false;
+    constructor() {
+        this.outputChannel = window.createOutputChannel("Gerrit");
+    }
+
+    log(value: string) {
+        let lines: string[] = value.split(new RegExp("\\n\\r?", "gmi"));
+        for (let i in lines) {
+            this.outputChannel.appendLine(lines[i]);
+        }
+    }
+
+}
