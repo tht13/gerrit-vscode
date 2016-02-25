38a54bb2b92826904d693333f7054553ef7a706b
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2016-02-25 20:49:51 +0100

    Implement controller in extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,35 @@
 "use strict";
 import * as vscode from "vscode";
 import { Gerrit } from "./gerrit/gerrit";
+import { GerritController } from "./gerrit/controller";
 
 export function activate(context: vscode.ExtensionContext) {
     let commands: vscode.Disposable[] = [];
+    let gerrit: Gerrit = new Gerrit("", "");
+    let controller: GerritController = new GerritController(gerrit);
+
     commands.push(
         vscode.commands.registerCommand("gerrit.checkoutBranch", () => {
             vscode.window.showInformationMessage("Gerrit: Check out branch");
+            controller.checkoutBranch();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.checkoutRevision", () => {
             vscode.window.showInformationMessage("Gerrit: Check out revision");
+            controller.checkoutRevision();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.commitAmend", () => {
             vscode.window.showInformationMessage("Gerrit: Commit amend");
+            controller.commitAmend();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.pushBranch", () => {
             vscode.window.showInformationMessage("Gerrit: Push to branch");
+            controller.commit();
         })
     );
 
