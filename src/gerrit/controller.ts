973d2fe6374494f0f8e3df3e2bae99d8c2649418
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2016-02-25 23:40:39 +0100

    Add input boxes for commands

--- src/gerrit/controller.ts
+++ src/gerrit/controller.ts
@@ -1,3 +1,4 @@
+import { window, InputBoxOptions } from "vscode";
 import { Gerrit } from "./gerrit";
 import { Ref } from "./ref";
 
@@ -7,22 +8,60 @@ export class GerritController {
     }
 
     public checkoutBranch() {
+        let options: InputBoxOptions = {
+            value: "master",
+            prompt: "The branch to checkout"
+        };
 
-        this.gerrit.checkoutBranch("master");
+        window.showInputBox(options).then(branch => {
+            this.gerrit.checkoutBranch(branch);
+        }, reason => {
+        });
     }
 
     public checkoutRevision() {
-        let newRef: Ref = new Ref(0, 0);
-        this.gerrit.setCurrentRef(newRef);
+        let revisionOptions: InputBoxOptions = {
+            placeHolder: "Ref Number",
+            validateInput: (value: string): string => {
+                if (isNaN(parseInt(value))) {
+                    return "Not a Number";
+                } else {
+                    return null;
+                }
+            },
+            prompt: "The revision to checkout"
+        };
+
+        window.showInputBox(revisionOptions).then(refString => {
+            let refId = parseInt(refString);
+            let patchsetOptions: InputBoxOptions = revisionOptions;
+            patchsetOptions.placeHolder = `Patchset for Ref: ${refString}`;
+            patchsetOptions.prompt = "The patchset to checkout";
+
+            window.showInputBox(patchsetOptions).then(patchString => {
+                let patchId = parseInt(patchString);
+                let newRef: Ref = new Ref(refId, patchId);
+                this.gerrit.setCurrentRef(newRef);
+            }, reason => {
+            });
+        }, reason => {
+        });
     }
 
     public commitAmend() {
-
+        // TODO: should not require new commit message
         this.gerrit.commit("", [""], true);
     }
 
     public commit() {
+        let options: InputBoxOptions = {
+            placeHolder: "Commit Message",
+            prompt: "The commit description"
+        };
 
-        this.gerrit.commit("", [""], false);
+        window.showInputBox(options).then(message => {
+            this.gerrit.commit(message, [""], false);
+        }, reason => {
+        });
     }
 }
