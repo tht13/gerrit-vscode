262f34f20cb7df8c4fdf0b3cd168e1bd96e826e0
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2016-03-01 17:51:20 +0100

    Remove information messages

--- src/extension.ts
+++ src/extension.ts
@@ -13,55 +13,46 @@ export function activate(context: vscode.ExtensionContext) {
 
     commands.push(
         vscode.commands.registerCommand("gerrit.checkoutBranch", () => {
-            vscode.window.showInformationMessage("Gerrit: Checkout branch");
             controller.checkoutBranch();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.checkoutRevision", () => {
-            vscode.window.showInformationMessage("Gerrit: Checkout revision");
             controller.checkoutRevision();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.cherrypickRevision", () => {
-            vscode.window.showInformationMessage("Gerrit: Cherrypick revision");
             controller.cherrypickRevision();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.cherrypickContinue", () => {
-            vscode.window.showInformationMessage("Gerrit: Cherrypick continue");
             controller.cherrypickContinue();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.commit", () => {
-            vscode.window.showInformationMessage("Gerrit: Commit");
             controller.commit();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.commitAmend", () => {
-            vscode.window.showInformationMessage("Gerrit: Commit amend");
             controller.commitAmend();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.pushBranch", () => {
-            vscode.window.showInformationMessage("Gerrit: Push to branch");
             controller.push();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.rebaseBranch", () => {
-            vscode.window.showInformationMessage("Gerrit: Rebase from branch");
             controller.rebase();
         })
     );
     commands.push(
         vscode.commands.registerCommand("gerrit.rebaseContinue", () => {
-            vscode.window.showInformationMessage("Gerrit: Rebase continue");
             controller.rebase();
         })
     );
