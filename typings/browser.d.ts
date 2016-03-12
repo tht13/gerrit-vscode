4948c5a8909e46918eff1ec41de264db62c80d37
Thomas Townsend <thomashaakon.townsend@emerson.com>
2016-03-12 12:46:51 +0100

    Removed typings due to build errors

--- typings/browser.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="browser/ambient/request-promise/index.d.ts" />
-/// <reference path="browser/definitions/request/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="main/ambient/request-promise/index.d.ts" />
-/// <reference path="main/definitions/request/index.d.ts" />
