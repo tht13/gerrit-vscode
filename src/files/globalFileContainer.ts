a92c212479ffb1044393f738d185973b3db98ed7
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2016-03-25 20:07:50 +0100

    Fix descriptor status

--- src/files/globalFileContainer.ts
+++ src/files/globalFileContainer.ts
@@ -94,7 +94,7 @@ export class GlobalFileContainer extends BasicFileContainer {
                 descriptors.push({
                     label: value.path,
                     path: value.path,
-                    description: gitCommon.GitStatus[status]
+                    description: gitCommon.GitStatus[value.status]
                 });
             });
         }
