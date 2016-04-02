de3bee7ab63f4e35b651f6cee2446445dd9c90e1
Thomas Haakon Townsend <thomas.haakon@gmail.com>
2016-04-02 14:30:56 +0200

    Add staged status map

--- src/git/common.ts
+++ src/git/common.ts
@@ -13,3 +13,12 @@ export enum GitStatus {
     STAGED_UNMERGED,
     UNTRACKED
 }
+
+export const GitStatedStatus: Map<string, GitStatus> = new Map<string, GitStatus>();
+GitStatedStatus.set("A", GitStatus.STAGED_ADDED);
+GitStatedStatus.set("C", GitStatus.STAGED_COPIED);
+GitStatedStatus.set("M", GitStatus.STAGED_MODIFIED);
+GitStatedStatus.set("R", GitStatus.STAGED_RENAMED);
+GitStatedStatus.set("T", GitStatus.STAGED_TYPE);
+GitStatedStatus.set("U", GitStatus.STAGED_UNMERGED);
+GitStatedStatus.set("X", GitStatus.STAGED_UNKNOWN);
